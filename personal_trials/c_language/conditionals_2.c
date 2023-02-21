7cb9e26eefdcd23cf47f7b7d6f6e78fb3020ce41
Wise-deldu <delduwise@gmail.com>
2023-02-21 23:00:45 +0000

    A code that employs if else statement

---
+++ personal_trials/c_language/conditionals_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - A code that employs if else staement
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n = 4;
+
+	if (n != 4)
+	{
+		printf("n is not 4\n");
+	}
+	else
+		printf("n is 4\n");
+	return (0);
+}
