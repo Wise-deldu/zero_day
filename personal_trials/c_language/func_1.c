7fcad4623ac17c5725eef2f4e24f6d3f7237db3b
Wise-deldu <delduwise@gmail.com>
2023-02-21 17:25:53 +0000

    An unsuccessful code to area of a rectangle

---
+++ personal_trials/c_language/func_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - A code to print the area of a rectangle
+ * Return: area (Successful)
+ */
+int main(void)
+{
+	int area;
+
+	area = length * breadth;
+
+	int l = 10, b = 5;
+
+	int area = areaOfRect(l, b);
+
+	printf("%d\n", area);
+	return (area);
+}
