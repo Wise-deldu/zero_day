7cdfcf9e2de3c74b8426ca93bdc8ee866257f7b4
Wise-deldu <delduwise@gmail.com>
2023-03-01 01:30:40 +0000

    Address of arrays

---
+++ personal_trials/c_language/pointers_and_arrays/array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - A code that print the various addresses of arrays
+* Return: 0
+*/
+
+int main(void)
+{
+	int arr[5] = {2, 4, 6, 7, 10};
+
+	printf("%p\n", &(arr[0]));
+	printf("%p\n", &(arr[1]));
+	printf("%p\n", &(arr[2]));
+	printf("%p\n", &(arr[3]));
+	printf("%p\n", &(arr[4]));
+	return (0);
+}
