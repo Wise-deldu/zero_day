f5611c28f5da09bf194d2c246df405652f8a4fd1
Wise-deldu <delduwise@gmail.com>
2023-03-07 01:59:21 +0000

    A code that prints multi-dimensional array

---
+++ personal_trials/c_language/multi_dimensional_array/arrays_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - a code that prints two-dimensional array
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	/* an array with 5 rows and 2 columns */
+	int a[5][2] = { {0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}};
+	int i, j;
+
+	/* output each array element's value */
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; j < 2; j++)
+		{
+			printf("a[%d][%d] = %d\n", i, j, a[i][j]);
+		}
+	}
+	return (0);
+}
