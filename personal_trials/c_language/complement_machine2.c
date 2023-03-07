42ae20f2efb1bac9b3fbadf283c0fa637e039d1d
Wise-deldu <delduwise@gmail.com>
2023-03-07 17:31:30 +0000

    Complement

---
+++ personal_trials/c_language/complement_machine2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - A program that prints a welcome message with complement.
+*
+* Return: 0 Always.
+*/
+
+int main(void)
+{
+	printf("Hello %s!\n", "Wise");
+
+	return (0);
+}
