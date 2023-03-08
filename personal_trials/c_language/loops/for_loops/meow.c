da034858a373b137697e6a5b3abd069393ba59b8
Wise-deldu <delduwise@gmail.com>
2023-03-08 10:17:29 +0000

    A code that prints meow 3 times

---
+++ personal_trials/c_language/loops/for_loops/meow.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - A code that print meow repeatedly for 3 times
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		printf("meow\n");
+	}
+	return (0);
+}
