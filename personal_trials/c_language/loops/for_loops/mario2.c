11562b3affc6c659cc0b71973f9ff8a838069374
Wise-deldu <delduwise@gmail.com>
2023-03-10 05:57:29 +0000

    A code that prints vertical blocks in mario

---
+++ personal_trials/c_language/loops/for_loops/mario2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - A code that prints blocks arranged vertically in mario
+  *
+  * Return: Always 0.
+  */
+
+int main(void)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		printf("##\n");
+	}
+	return (0);
+}
