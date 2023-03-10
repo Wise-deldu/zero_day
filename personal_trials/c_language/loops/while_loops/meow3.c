c15877c9c2e416f494d5b62d81f6bc6506179890
Wise-deldu <delduwise@gmail.com>
2023-03-10 05:22:37 +0000

    A program that prints meow to infinity

---
+++ personal_trials/c_language/loops/while_loops/meow3.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+/**
+  * main - A code that prints meow to infinity.
+  *
+  * Return: Always 0.
+  */
+
+int main(void)
+{
+	while (true)
+		printf("meow\n");
+	return (0);
+}
