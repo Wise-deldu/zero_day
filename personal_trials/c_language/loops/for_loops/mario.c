06375d0db226cb6f39778e76101cbcfac90094a9
Wise-deldu <delduwise@gmail.com>
2023-03-10 05:52:38 +0000

    A code that prints ???? for times

---
+++ personal_trials/c_language/loops/for_loops/mario.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - A code that prints the four question marks (?) in mario game
+  *
+  * Return: Always 0.
+  */
+
+int main(void)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		printf("?");
+	}
+	printf("\n");
+
+	return (0);
+}
