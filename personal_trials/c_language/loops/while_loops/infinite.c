3f081eca68b9c3d952f50606ece16c7bbfa07c9c
Wise-deldu <delduwise@gmail.com>
2023-03-12 22:56:57 +0000

    An infinite loop

---
+++ personal_trials/c_language/loops/while_loops/infinite.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - A program that prints an infite loop
+  *
+  * Return: Always 0.
+  */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 10)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
