e68b2b73d3ae24cab3aa606fcef83fd69fff377c
Wise-deldu <delduwise@gmail.com>
2023-03-13 09:17:46 +0000

    decrement with float

---
+++ personal_trials/c_language/loops/while_loops/decrement2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - A program that prints a while loop using decrement and float.
+  *
+  * Return: Always 0.
+  */
+
+int main(void)
+{
+	float k = 10.0;
+
+	while (k <= 10.5)
+	{
+		printf("Raindrops on roses...");
+		printf("...and whiskers on kittens\n");
+		k = k + 0.1;
+	}
+	return (0);
+}
