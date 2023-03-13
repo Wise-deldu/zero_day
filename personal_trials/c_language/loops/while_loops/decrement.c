132a9011cfdb156038992f3700d4cfb92f09e92f
Wise-deldu <delduwise@gmail.com>
2023-03-13 08:07:32 +0000

    Decrement

---
+++ personal_trials/c_language/loops/while_loops/decrement.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - A program that prints a while loop with a decrement.
+  *
+  * Return: Always 0.
+  */
+
+int main(void)
+{
+	int m = 10;
+
+	while (m >= 1)
+	{
+		printf("Make the computer literate!\n");
+
+		m = m - 1;
+	}
+	return (0);
+}
