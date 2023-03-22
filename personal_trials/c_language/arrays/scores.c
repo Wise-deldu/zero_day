7393262549fda2d4966e481a33321519d0915ed5
Wise-deldu <delduwise@gmail.com>
2023-03-22 10:45:29 +0000

    A program that finds an average scores

---
+++ personal_trials/c_language/arrays/scores.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - A program that prints the average of three marks
+  *
+  * Return: Always 0.
+  */
+
+int main(void)
+{
+	int score1 = 73;
+	int score2 = 72;
+	int score3 = 33;
+
+	printf("Average: %i\n", (score1 + score2 + score3) / 3);
+
+	return (0);
+}
